d16412db69590e4f6487716185adcda669f3d10c
JorgeLant <jorgelantiguad05@gmail.com>
2026-06-08 15:09:52 -0400

    Agregando atributos correspondientes

--- Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/Models/Entities/Audiencia.cs
+++ Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/Models/Entities/Audiencia.cs
@@ -2,5 +2,11 @@
 {
     public class Audiencia
     {
+        public int Id { get; set; }
+        public int CasoAsociado { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeOnly Hora { get; set; }
+        public string Tribunal { get; set; }
+        public string Estado { get; set; }
     }
 }
--- Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/Models/Entities/Caso.cs
+++ Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/Models/Entities/Caso.cs
@@ -2,5 +2,16 @@
 {
     public class Caso
     {
+        public int Id { get; set; }
+        public string NumeroExpediente { get; set; }
+        public int ClienteAsociado { get; set; }
+        public int AbogadoAsociado { get; set; }
+        public string Titulo { get; set; }
+        public string TipoCaso { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaApertura { get; set; }
+        public string Estado { get; set; }
+        public string Tribunal { get; set; }
+        public string ParteContraria { get; set; }
     }
 }
