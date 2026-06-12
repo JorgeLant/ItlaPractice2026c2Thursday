3ce06ea5d87723b6025c408895b017c6105aa5cb
JorgeLant <jorgelantiguad05@gmail.com>
2026-06-12 02:22:38 -0400

    Conexion Con DB

--- Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/AbogadoController.cs
+++ Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/AbogadoController.cs
@@ -1,6 +1,8 @@
-﻿using Gestor_de_Datos_Oficina_Legal.Models.DTOs;
+﻿using Gestor_de_Datos_Oficina_Legal.Contexts;
+using Gestor_de_Datos_Oficina_Legal.Models.DTOs;
 using Gestor_de_Datos_Oficina_Legal.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gestor_de_Datos_Oficina_Legal.Controllers
 {
@@ -8,17 +10,18 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
     [Route("api/[controller]")]
     public class AbogadoController : ControllerBase
     {
-        private static readonly List<Abogado> abogados = new List<Abogado>
+        private readonly ConexionDB _context;
+
+        public AbogadoController(ConexionDB context)
         {
-            new Abogado {Id = 1, Nombre = "Darling Quezada", Cedula = "402,0000001-1", Exequatur = "EXE-2020-001", NumeroTelefonico = "809-000-0001", CorreoElectronico = "Darlingquezada@gmail.com", Direccion = "Av.Charles de gaulle, Santo Domingo Este", Especialidad = "Laboral", FechaRegistro = new DateTime(2019,12,01), Estado = "Inactivo"},
-            new Abogado {Id = 2, Nombre = "Wascar Mendieta", Cedula = "402,0000002-1", Exequatur = "EXE-2018-002", NumeroTelefonico = "829-000-0001", CorreoElectronico = "Wascarmendieta@gmail.com", Direccion = "Av.Monumental, Santo Domingo Este", Especialidad = "Penal", FechaRegistro = new DateTime(2014,12,01), Estado = "Activo"},
-            new Abogado {Id = 3, Nombre = "Mariana Gonzalez", Cedula = "402,0000002-1", Exequatur = "EXE-2021-003", NumeroTelefonico = "849-000-0001", CorreoElectronico = "Marianagonzalez@gmail.com", Direccion = "Av.Winston Churchill, Distrito Nacional, Santo Domingo ", Especialidad = "Civil", FechaRegistro = new DateTime(2021,04,21), Estado = "Activo"}
-        };
+            _context = context;
+        }
 
         [HttpGet]
         [Route("Get-Abogados")]
-        public ActionResult<IEnumerable<Abogado>> GetAbogado()
+        public async Task<ActionResult<IEnumerable<Abogado>>> GetAbogado(ConexionDB context)
         {
+            var abogados = await _context.Abogados.AsNoTracking().ToListAsync();
             return Ok(abogados);
         }
 
@@ -26,7 +29,7 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
         [Route("GetId-Abogado")]
         public ActionResult<Abogado> GetByIdAbogado(int id)
         {
-            var abogado = abogados.FirstOrDefault(x => x.Id == id);
+            var abogado =  _context.Abogados.FirstOrDefault(x => x.Id == id);
             if (abogado == null)
             {
                 return NotFound();
@@ -36,11 +39,11 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
 
         [HttpPost]
         [Route("Post-Abogado")]
-        public ActionResult<Abogado> CreateAbogado(AbogadoDTO request)
+        public async Task<ActionResult<Abogado>> CreateAbogado(AbogadoDTO request)
         {
             var abogado = new Abogado
             {
-                Id = request.Id,
+
                 Nombre = request.Nombre,
                 Cedula = request.Cedula,
                 Exequatur = request.Exequatur,
@@ -51,16 +54,17 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
                 FechaRegistro = request.FechaRegistro,
                 Estado = request.Estado,
             };
-            abogado.Id = abogados.Max(x => x.Id) + 1;
-            abogados.Add(abogado);
+
+            _context.Abogados.Add(abogado);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetByIdAbogado), new { id = abogado.Id }, abogado);
         }
 
         [HttpPut]
         [Route("PutID-Abogado")]
-        public ActionResult Update(int id, Abogado updatedAbogado)
+        public async Task<ActionResult> Update(int id, Abogado updatedAbogado)
         {
-            var abogado = abogados.FirstOrDefault(x => x.Id == id);
+            var abogado =  _context.Abogados.FirstOrDefault(x => x.Id == id);
             if (abogado == null)
             {
                 return NotFound();
@@ -68,20 +72,22 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
             abogado.CorreoElectronico = updatedAbogado.CorreoElectronico;
             abogado.NumeroTelefonico = updatedAbogado.NumeroTelefonico;
             abogado.Direccion = updatedAbogado.Direccion;
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
         [HttpDelete]
         [Route("Delete-Abogado")]
 
-        public ActionResult DeleteAb(int id)
+        public async Task<ActionResult> DeleteAb(int id)
         {
-            var abogado = abogados.FirstOrDefault(x => x.Id == id);
+            var abogado = _context.Abogados.FirstOrDefault(x => x.Id == id);
             if (abogado == null)
             {
                 return NotFound();
             }
-            abogados.Remove(abogado);
+            _context.Abogados.Remove(abogado);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
--- Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/AudienciaController.cs
+++ Gestor de Datos Oficina Legal/Gestor de Datos Oficina Legal/Controllers/AudienciaController.cs
@@ -1,6 +1,8 @@
-﻿using Gestor_de_Datos_Oficina_Legal.Models.DTOs;
+﻿using Gestor_de_Datos_Oficina_Legal.Contexts;
+using Gestor_de_Datos_Oficina_Legal.Models.DTOs;
 using Gestor_de_Datos_Oficina_Legal.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gestor_de_Datos_Oficina_Legal.Controllers
 {
@@ -8,25 +10,26 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
     [Route("api/[controller]")]
     public class AudienciaController: ControllerBase
     {
-        private static readonly List<Audiencia> audiencias = new List<Audiencia>
+        private readonly ConexionDB _context;
+
+        public AudienciaController(ConexionDB context)
         {
-            new Audiencia {Id = 1, CasoAsociado = 3, Fecha = new DateTime(2026,08,14), Hora = new TimeOnly(16,30), Tribunal = "Palacio de Justicia de la Provincia Santo Domingo", Estado = "Activo"},
-            new Audiencia {Id = 2, CasoAsociado = 1, Fecha = new DateTime(2024,05,30), Hora = new TimeOnly(10,00), Tribunal = "Edificio Suprema Corte de Justicia", Estado = "Cerrado"},
-            new Audiencia {Id = 3, CasoAsociado = 2, Fecha = new DateTime(2025,11,02), Hora = new TimeOnly(00,00), Tribunal = "Palacio de justicia de Santiago", Estado = "Suspendido"}
-        };
+            _context = context;
+        }
 
         [HttpGet]
         [Route("Get-Audiencias")]
-        public ActionResult<IEnumerable<Audiencia>> GetAudiencia()
+        public async Task<ActionResult<IEnumerable<Audiencia>>> GetAudiencia()
         {
-            return Ok(audiencias);
+            var audiencia = await _context.Audiencias.AsNoTracking().ToListAsync();
+            return Ok(_context.Audiencias);
         }
 
         [HttpGet]
         [Route("GetID-Audiencia")]
         public ActionResult<Audiencia> GetByIdAudiencia(int id)
         {
-            var audiencia = audiencias.FirstOrDefault(x => x.Id == id);
+            var audiencia = _context.Audiencias.FirstOrDefault(x => x.Id == id);
             if (audiencia == null)
             {
                 return NotFound();
@@ -36,27 +39,27 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
 
         [HttpPost]
         [Route("Post-Audiencia")]
-        public ActionResult<Audiencia> CreateAudiencia(AudienciaDTO request)
+        public async Task<ActionResult<Audiencia>> CreateAudiencia(AudienciaDTO request)
         {
             var audiencia = new Audiencia
             {
-                Id = request.Id,
                 CasoAsociado=request.CasoAsociado,
                 Fecha = request.Fecha,
                 Hora = request.Hora,
                 Tribunal=request.Tribunal,
                 Estado = request.Estado,
             };
-            audiencia.Id = audiencias.Max(x => x.Id) + 1;
-            audiencias.Add(audiencia);
+
+            _context.Audiencias.Add(audiencia);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetByIdAudiencia), new { id = audiencia.Id }, audiencia);
         }
 
         [HttpPut]
         [Route("Put-Audiencia")]
-        public ActionResult Update(int id, Audiencia updatedAudiencia)
+        public async Task<ActionResult> Update(int id, Audiencia updatedAudiencia)
         {
-            var audiencia = audiencias.FirstOrDefault(x => x.Id == id);
+            var audiencia = _context.Audiencias.FirstOrDefault(x => x.Id == id);
             if (audiencia == null)
             {
                 return NotFound();
@@ -65,19 +68,21 @@ namespace Gestor_de_Datos_Oficina_Legal.Controllers
             audiencia.Fecha = updatedAudiencia.Fecha;
             audiencia.Hora = updatedAudiencia.Hora;
             audiencia.Tribunal = updatedAudiencia.Tribunal;
+            await _context.SaveChangesAsync();
             return NoContent();
         }
 
         [HttpDelete]
         [Route("Delete-Audiencia")]
-        public ActionResult DeleteAu(int id)
+        public async Task<ActionResult> DeleteAu(int id)
         {
-            var audiencia = audiencias.FirstOrDefault(x => x.Id == id);
+            var audiencia = _context.Audiencias.FirstOrDefault(x => x.Id == id);
             if (audiencia == null)
             {
                 return NotFound();
             }
-            audiencias.Remove(audiencia);
+            _context.Audiencias.Remove(audiencia);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
